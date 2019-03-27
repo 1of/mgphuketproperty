4cf60f29ad022d1c910f66dcafb3081eca097283
Silifonov Alexey <silifonov@gmail.com>
2019-03-27 14:50:06 +0200

    show modal when contact form is send

--- wp-content/themes/quality/inc/modal/contact-modal.php
+++ wp-content/themes/quality/inc/modal/contact-modal.php
@@ -7,8 +7,15 @@
                 <h4 class="modal-title" id="myModalLabel">Заполните форму</h4>
             </div>
             <div class="modal-body">
-                <?php echo do_shortcode('[contact-form-7 id="642" title="Модальное окно"]'); ?>
+                <?php echo do_shortcode('[contact-form-7 id="689" title="Модальное окно"]'); ?>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+  document.addEventListener( 'wpcf7mailsent', function( event ) {
+    jQuery('#contact-modal').modal('hide');
+    jQuery('#send-ok-modal').modal('show'); //show confirmation modal
+  }, false );
+</script>
--- wp-content/themes/quality/inc/modal/sending-confirmation-modal.php
+++ wp-content/themes/quality/inc/modal/sending-confirmation-modal.php
@@ -2,15 +2,14 @@
 <div class="modal fade" id="send-ok-modal" tabindex="-1" role="dialog" aria-labelledby="send-ok-modal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header mb-10">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="send-ok-modal">Заполните форму</h4>
             </div>
             <div class="modal-body">
                 <h3>Cпасибо за обращение, ваш запрос отправлен!</h3>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                <button type="button" class="btn btn-default default-btn" data-dismiss="modal">Закрыть</button>
             </div>
         </div>
     </div>
